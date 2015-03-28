3f0b0629238974b3a7205cb0da08e44ac0f70d31
RickR <rrein@akamai.com>
2015-03-28 12:45:20 -0400

    kill stuff

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+pkill -9 'tmux -d -s be'
+
 source /usr/local/rvm/scripts/rvm
 rvm use 2.2
 
-tmux new-session -d -s default
-tmux new-window -t default:1 -n 'src' 'ruby bbqio_be.rb'
+tmux new-session -d -s be
+tmux new-window -t be:1 -n 'src' 'ruby bbqio_be.rb'
