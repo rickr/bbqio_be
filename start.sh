e3b43f57a485ba635cb4813e540668d2ae139acf
RickR <rrein@akamai.com>
2015-03-28 14:09:35 -0400

    2.1.5

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 kill -9 $(ps -ef | grep '[n]ew-session -d -s be' | awk ' { print $2 } ')
 
 source /usr/local/rvm/scripts/rvm
-rvm use 2.2
+rvm use 2.1.5
 
 tmux new-session -d -s default
 tmux new-window -t default:1 -n 'src' 'ruby bbqio_be.rb'
