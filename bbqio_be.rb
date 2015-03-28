f09924a41b48fc8793a98874d4f472f5f85b545e
RickR <rrein@akamai.com>
2015-03-28 14:07:07 -0400

    clean up view

--- bbqio_be.rb
+++ bbqio_be.rb
@@ -32,11 +32,11 @@ get '/sensor_data' do
   if request.websocket?
     request.websocket do |ws|
       ws.onopen do
-        ws.send("Hello World!")
         settings.sockets << ws
       end
 
       ws.onmessage do |msg|
+        puts msg
         EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
       end
 
