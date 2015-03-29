dc707241f5d95524d0f808e229d7f54720834d28
RickR <rrein@akamai.com>
2015-03-28 22:22:37 -0400

    i forgot what i did

--- test_data.sh
+++ test_data.sh
@@ -1,8 +1,16 @@
 while true; do
   chamberTemp=$(( ( RANDOM % 20 )  + 250 ))
   fanStatus=$(( ( RANDOM % 1 ) ))
-  smokeLevel=$(( ( RANDOM % 20 )  + 250 ))
+  smokeLevel=$(( ( RANDOM % 30 )  + 250 ))
   meatTemp=$(( ( RANDOM % 20 )  + 100 ))
-  curl --data "timestamp=$(date +%s)&chamberTemp=${chamberTemp}&fanStatus=${fanStatus}&smokeLevel=${smokeLevel}&meatTemp=${meatTemp}" http://localhost:4567/sensor_data; echo
+
+  fudgeValue=$(echo "$(( RANDOM % 10 )) * .01" | bc)
+  humidityValue=$(echo ".77 + $fudgeValue" | bc )
+
+  fudgeValue=$(echo "$(( RANDOM % 10 )) * .01" | bc)
+  smokeValue=$(echo ".57 + $fudgeValue" | bc )
+
+  curl --data "timestamp=$(date +%s)&chamberTemp=${chamberTemp}&fanStatus=${fanStatus}&smokeLevel=${smokeLevel}&meatTemp=${meatTemp}&humidityValue=${humidityValue}&smokeValue=${smokeValue}" \
+  http://localhost:4567/sensor_data; echo
   sleep 1
 done
